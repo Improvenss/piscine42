2c1639d9661756aba0115f2008651e1cd147d502
Gorkem Sever <gsever@k2m03s06.42kocaeli.com.tr>
2021-11-25 13:25:40 +0300

    c00 ex05 yapıldı

---
+++ ft_print_comb/ft_print_comb/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ft_print_comb
+//
+//  Created by Gorkem Sever on 11/25/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

619a90379086a619ed2b2e4d844356a44da1afd2
gorkemsever <gorkem_se_98@hotmail.com>
2021-12-09 23:17:33 +0300

    exam_samples_added

---
+++ exam_studying/inter.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/09 19:26:47 by gsever            #+#    #+#             */
+/*   Updated: 2021/12/09 20:00:47 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		check_doubles(char *str, char c, int pos)
+{
+	int i;
+
+	i = 0;
+	while (i < pos)
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+void	inter(char *str, char *str1)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str1[j] != '\0')
+		{
+			if (str[i] == str1[j])
+			{
+				if (check_doubles(str, str[i], i) == 1)
+				{
+					write(1, &str[i], 1);
+					break;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+		inter(av[1], av[2]);
+	write(1, "\n", 1);
+	return (0);
+}
